3096d69133257db5c7e01cc6c80fe98257e1318c
Kevzlou7979 <kevzlou7979@gmail.com>
2015-12-02 21:34:38 +0800

    Implement Pure CSS Files to Steppers.
    Added Steppers and Step Complex Widget.
    Added Active Mixins for Steppers.
    Following Material Specs about the properties of Steppers and Steps.
    Added nextStep() and prevStep() for Stepper Main component to easily
    control Steps.
    Debugged the Logical code of Steppers next / prev methods.
    Migration of Steppers into gwt-material-addins.
    Added goToStep(int step) method, usually used to Non Linear Steppers.

---
+++ src/main/java/gwt/material/design/addins/client/ui/MaterialStep.java
@@ -0,0 +1,128 @@
+package gwt.material.design.addins.client.ui;
+
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.Widget;
+import gwt.material.design.addins.client.base.mixin.ActiveMixin;
+import gwt.material.design.client.base.ComplexWidget;
+import gwt.material.design.client.base.HasActive;
+import gwt.material.design.client.ui.html.Div;
+
+//@formatter:off
+
+/**
+ * Material Step is a child element of Material Stepper, sometimes called Stepper Item, used to indicate the active
+ * inactive items on the Stepper Component.
+ *
+ * <h3>XML Namespace Declaration</h3>
+ * <pre>
+ * {@code
+ * xmlns:m.addins='urn:import:gwt.material.design.addins.client.ui'
+ * }
+ * </pre>
+ *
+ * <h3>UiBinder Usage:</h3>
+ *
+ * <pre>
+ * {@code
+ *   <m.addins:MaterialStep step="1" title="Name of Step 1">
+ *       <m.addins:MaterialPanel width="100%" height="300px" backgroundColor="grey lighten-2"/>
+ *       <m.addins:MaterialButton ui:field="btnContinue1" text="Continue to Step 2" grid="l4" marginTop="12" backgroundColor="blue" textColor="white" waves="DEFAULT"/>
+ *       <m.addins:MaterialButton ui:field="btnPrev1" text="Cancel" grid="l4" marginTop="12" type="FLAT" waves="DEFAULT"/>
+ *   </m.addins:MaterialStep>
+ * }
+ * </pre>
+ *
+ * @author kevzlou7979
+ * @see <a href="http://gwtmaterialdesign.github.io/gwt-material-demo/snapshot/#steppers">Material Steppers</a>
+ */
+// @formatter:on
+public class MaterialStep extends ComplexWidget implements HasActive {
+
+    private int step;
+    private String title;
+
+    // containers
+    private Div conCircle = new Div();
+    private Div conBody = new Div();
+
+    // elements
+    private Div divCircle = new Div();
+    private Div divLine = new Div();
+    private Div divTitle = new Div();
+    private Div divBody = new Div();
+
+    private final ActiveMixin<MaterialStep> activeMixin = new ActiveMixin<>(this);
+
+    public MaterialStep() {
+        super(Document.get().createDivElement());
+        setStyleName("step");
+
+        super.add(conCircle);
+        conCircle.add(divCircle);
+        conCircle.add(divLine);
+
+        super.add(conBody);
+        conBody.add(divTitle);
+        conBody.add(divBody);
+
+        divCircle.setStyleName("circle");
+        divLine.setStyleName("line");
+        divTitle.setStyleName("title");
+        divBody.setStyleName("body");
+    }
+
+    @Override
+    public void add(Widget child) {
+        divBody.add(child);
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+        divCircle.getElement().setInnerHTML(String.valueOf(step));
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+        divTitle.getElement().setInnerHTML(title);
+    }
+
+    @Override
+    public void setActive(boolean active) {
+        activeMixin.setActive(active);
+    }
+
+    @Override
+    public boolean isActive() {
+        return activeMixin.isActive();
+    }
+}
---
+++ src/main/java/gwt/material/design/addins/client/ui/MaterialStepper.java
@@ -0,0 +1,145 @@
+package gwt.material.design.addins.client.ui;
+
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.Widget;
+import gwt.material.design.client.base.ComplexWidget;
+
+//@formatter:off
+
+/**
+ * Steppers convey progress through numbered steps. They may also be used for navigation.
+ *
+ * <h3>XML Namespace Declaration</h3>
+ * <pre>
+ * {@code
+ * xmlns:m.addins='urn:import:gwt.material.design.addins.client.ui'
+ * }
+ * </pre>
+ *
+ * <h3>UiBinder Usage:</h3>
+ *
+ * <pre>
+ * {@code
+ *  <m.addins:MaterialStepper ui:field="stepper">
+ *      <m.addins:MaterialStep step="1" title="Name of Step 1">
+ *          <m.addins:MaterialPanel width="100%" height="300px" backgroundColor="grey lighten-2"/>
+ *          <m.addins:MaterialButton ui:field="btnContinue1" text="Continue to Step 2" grid="l4" marginTop="12" backgroundColor="blue" textColor="white" waves="DEFAULT"/>
+ *          <m.addins:MaterialButton ui:field="btnPrev1" text="Cancel" grid="l4" marginTop="12" type="FLAT" waves="DEFAULT"/>
+ *      </m.addins:MaterialStep>
+ *  &lt;!-- Other Step components here -->
+ *  </m.addins:MaterialStepper>
+ * }
+ * </pre>
+ *
+ * @author kevzlou7979
+ * @see <a href="http://gwtmaterialdesign.github.io/gwt-material-demo/snapshot/#steppers">Material Steppers</a>
+ */
+// @formatter:on
+public class MaterialStepper extends ComplexWidget {
+
+    private int totalSteps = 0;
+    private int currentStep = 0;
+
+    public MaterialStepper (){
+        super(Document.get().createDivElement());
+        setStyleName("stepper");
+    }
+
+    @Override
+    protected void onLoad() {
+        super.onLoad();
+        goToStep(1);
+    }
+
+    /**
+     * Go to next Step , used by linear stepper
+     */
+    public void nextStep() {
+        if(currentStep >= getWidgetCount() - 1){
+            GWT.log("You have reach the maximum step.");
+        }else{
+            Widget w = getWidget(currentStep);
+            if(w instanceof MaterialStep){
+                MaterialStep step = (MaterialStep) w;
+                step.setActive(false);
+
+                // next step
+                int nextStepIndex = getWidgetIndex(step) + 1;
+                MaterialStep nextStep = (MaterialStep) getWidget(nextStepIndex);
+                nextStep.setActive(true);
+            }
+            currentStep++;
+        }
+    }
+
+    /**
+     * Go to previous step , used by linear stepper.
+     */
+    public void prevStep() {
+        if(currentStep > 0) {
+            Widget w = getWidget(currentStep);
+            if (w instanceof MaterialStep) {
+                MaterialStep step = (MaterialStep) w;
+                step.setActive(false);
+
+                // next step
+                int prevStepIndex = getWidgetIndex(step) - 1;
+                MaterialStep prevStep = (MaterialStep) getWidget(prevStepIndex);
+                prevStep.setActive(true);
+            }
+            currentStep--;
+        }else{
+            GWT.log("You have reach the minimum step.");
+        }
+    }
+
+    /**
+     * Go to specific step manually by setting which step you want to go.
+     * @param step
+     */
+    public void goToStep(int step){
+        totalSteps = getWidgetCount();
+        for(int i = 0; i < totalSteps; i++){
+            Widget w = getWidget(i);
+            if(w instanceof MaterialStep){
+                ((MaterialStep) w).setActive(false);
+            }
+        }
+
+        Widget w = getWidget(step - 1);
+        if(w instanceof MaterialStep){
+            ((MaterialStep) w).setActive(true);
+        }
+        currentStep = step - 1;
+    }
+
+    public void setCurrentStep(int currentStep) {
+        this.currentStep = currentStep;
+    }
+
+    public int getCurrentStep() {
+        return currentStep;
+    }
+}
