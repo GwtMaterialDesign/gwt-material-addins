fb68810a8fc12c50200b32f8204e5f6de9c14ed5
Kevzlou7979 <kevzlou7979@gmail.com>
2016-02-23 14:00:56 +0800

    WIP - MaterialSwipeablePanel implementation

---
+++ src/main/java/gwt/material/design/addins/client/base/HasSwipeable.java
@@ -0,0 +1,49 @@
+package gwt.material.design.addins.client.base;
+
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.HasHandlers;
+import com.google.gwt.user.client.ui.Widget;
+import gwt.material.design.addins.client.events.SwipeLeftEvent;
+import gwt.material.design.addins.client.events.SwipeRightEvent;
+
+public interface HasSwipeable<T> extends HasHandlers {
+
+    /**
+     * Initialize the swipeable component
+     */
+    void initSwipeable(Element element, Widget target);
+
+    /**
+     * Add swipe left handler
+     * @param handler
+     */
+    HandlerRegistration addSwipeLeft(SwipeLeftEvent.SwipeLeftHandler<T> handler);
+
+    /**
+     * Add swipe right handler
+     * @param handler
+     */
+    HandlerRegistration addSwipeRight(SwipeRightEvent.SwipeRightHandler<T> handler);
+
+}
---
+++ src/main/java/gwt/material/design/addins/client/events/SwipeLeftEvent.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package gwt.material.design.addins.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import gwt.material.design.addins.client.base.HasSwipeable;
+
+/**
+ * Represents a swipe left event.
+ * @author kevzlou7979
+ * @param <T> the type being swiped to left
+ */
+public class SwipeLeftEvent<T> extends GwtEvent<SwipeLeftEvent.SwipeLeftHandler<T>> {
+
+    private static Type<SwipeLeftHandler<?>> TYPE;
+
+    public interface SwipeLeftHandler<T> extends EventHandler {
+        void onSwipeLeft(SwipeLeftEvent<T> event);
+    }
+
+    public static <T> void fire(HasSwipeable<T> source, T target) {
+        if (TYPE != null) {
+            SwipeLeftEvent<T> event = new SwipeLeftEvent<T>(target);
+            source.fireEvent(event);
+        }
+    }
+
+    public static Type<SwipeLeftHandler<?>> getType() {
+        return TYPE != null ? TYPE : (TYPE = new Type<SwipeLeftHandler<?>>());
+    }
+
+    private final T target;
+
+    protected SwipeLeftEvent(T target) {
+        this.target = target;
+    }
+
+    @Override
+    public final Type<SwipeLeftHandler<T>> getAssociatedType() {
+        return (Type) TYPE;
+    }
+
+    public T getTarget() {
+        return target;
+    }
+
+    @Override
+    protected void dispatch(SwipeLeftHandler<T> handler) {
+        handler.onSwipeLeft(this);
+    }
+}
