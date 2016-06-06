cb075ba7fd2e127ce115f1c8a69a584f08086eb5
masterdany88 <masterdany88@gmail.com>
2016-06-06 10:07:36 +0200

    Update UploadResponse.java

--- src/main/java/gwt/material/design/addins/client/fileuploader/base/UploadResponse.java
+++ src/main/java/gwt/material/design/addins/client/fileuploader/base/UploadResponse.java
@@ -29,16 +29,18 @@ public class UploadResponse {
 
     private String code;
     private String message;
+    private String body;
 
-    public UploadResponse(String code, String message) {
+
+    public UploadResponse(String code, String message, String body) {
         this.code = code;
         this.message = message;
+        this.body = body;
     }
 
     public String getCode() {
         return code;
     }
-
     public void setCode(String code) {
         this.code = code;
     }
@@ -46,8 +48,14 @@ public class UploadResponse {
     public String getMessage() {
         return message;
     }
-
     public void setMessage(String message) {
         this.message = message;
+    }
+    
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
     }
 }
