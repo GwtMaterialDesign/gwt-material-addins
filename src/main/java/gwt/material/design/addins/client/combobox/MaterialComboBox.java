29afb968511cc7801822fa9a7cc19344b3fd1989
Kevzlou7979 <kevzlou7979@gmail.com>
2016-07-04 15:36:41 +0800

    Initial Delivery of MaterialComboBox - Basic and Multiple selection feature.

---
+++ src/main/java/gwt/material/design/addins/client/combobox/MaterialComboBox.java
@@ -0,0 +1,94 @@
+package gwt.material.design.addins.client.combobox;
+
+/*
+ * #%L
+ * GwtMaterial
+ * %%
+ * Copyright (C) 2015 - 2016 GwtMaterialDesign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style;
+import gwt.material.design.addins.client.MaterialAddins;
+import gwt.material.design.client.MaterialDesignBase;
+import gwt.material.design.client.base.MaterialWidget;
+
+import static gwt.material.design.addins.client.combobox.js.JsComboBox.$;
+
+//@formatter:off
+
+/**
+ * ComboBox component used on chat module
+ *
+ * <h3>XML Namespace Declaration</h3>
+ * <pre>
+ * {@code
+ * xmlns:ma='urn:import:gwt.material.design.addins.client'
+ * }
+ * </pre>
+ *
+ * <h3>UiBinder Usage:</h3>
+ * <pre>
+ * {@code
+ * <combobox:MaterialComboBox>
+ *   <m:html.Option value="1" text="Sample 1"/>
+ *   <m:html.Option value="2" text="Sample 2"/>
+ *   <m:html.Option value="3" text="Sample 3"/>
+ * </combobox:MaterialComboBox>
+ * }
+ * </pre>
+ *
+ * @author kevzlou7979
+ * @see <a href="http://gwtmaterialdesign.github.io/gwt-material-demo/#combobox">Material ComboBox</a>
+ */
+//@formatter:on
+public class MaterialComboBox extends MaterialWidget {
+
+    static {
+        if(MaterialAddins.isDebug()) {
+            MaterialDesignBase.injectDebugJs(MaterialComboBoxDebugClientBundle.INSTANCE.select2DebugJs());
+            MaterialDesignBase.injectCss(MaterialComboBoxDebugClientBundle.INSTANCE.select2DebugCss());
+        } else {
+            MaterialDesignBase.injectJs(MaterialComboBoxClientBundle.INSTANCE.select2Js());
+            MaterialDesignBase.injectCss(MaterialComboBoxClientBundle.INSTANCE.select2Css());
+        }
+    }
+
+    public MaterialComboBox() {
+        super(Document.get().createSelectElement());
+    }
+
+    @Override
+    protected void onLoad() {
+        super.onLoad();
+        initialize();
+    }
+
+    protected void initialize() {
+        $(getElement()).select2();
+        setGwtDisplay(Style.Display.BLOCK);
+    }
+
+    public void setMultiple(boolean value) {
+        if(value) {
+            $(getElement()).attr("multiple", "multiple");
+        }else {
+            $(getElement()).removeAttr("multiple");
+        }
+    }
+
+}
