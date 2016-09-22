30d8da5648fe980d779d5fb30d7fd2aebe70b185
Kevzlou7979 <kevzlou7979@gmail.com>
2016-09-22 20:17:41 +0800

    Removed unused imports.

--- src/main/java/gwt/material/design/addins/client/richeditor/MaterialRichEditor.java
+++ src/main/java/gwt/material/design/addins/client/richeditor/MaterialRichEditor.java
@@ -19,16 +19,11 @@
  */
 package gwt.material.design.addins.client.richeditor;
 
-import com.google.gwt.core.client.JsArrayString;
-import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.HasHTML;
-import com.sun.prism.Material;
 import gwt.material.design.addins.client.MaterialAddins;
 import gwt.material.design.addins.client.richeditor.base.HasPasteHandlers;
 import gwt.material.design.addins.client.richeditor.base.MaterialRichEditorBase;
