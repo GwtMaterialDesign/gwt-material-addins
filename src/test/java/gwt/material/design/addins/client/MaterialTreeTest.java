2a782924576a8f6243bf432257c273ca4460e9f6
Kevzlou7979 <kevzlou7979@gmail.com>
2016-10-12 21:32:50 +0800

    TreeItem - Updated super method of initial Class, allow access to getSpan and getImage
    Tree Test (WIP) - checkStructure and checkChildren

--- src/test/java/gwt/material/design/addins/client/MaterialTreeTest.java
+++ src/test/java/gwt/material/design/addins/client/MaterialTreeTest.java
@@ -19,19 +19,72 @@
  */
 package gwt.material.design.addins.client;
 
+import com.google.gwt.user.client.ui.RootPanel;
 import gwt.material.design.addins.client.base.MaterialAddinsTest;
+import gwt.material.design.addins.client.base.constants.AddinsCssName;
 import gwt.material.design.addins.client.tree.MaterialTree;
+import gwt.material.design.addins.client.tree.MaterialTreeItem;
 import gwt.material.design.client.base.MaterialWidget;
+import gwt.material.design.client.constants.IconType;
+import gwt.material.design.client.ui.MaterialIcon;
+import gwt.material.design.client.ui.MaterialImage;
+import gwt.material.design.client.ui.html.Span;
 
 public class MaterialTreeTest extends MaterialAddinsTest {
 
     public void init() {
         MaterialTree tree = new MaterialTree();
         checkWidget(tree);
+        checkStructure(tree);
+    }
+
+    protected <T extends MaterialTree> void checkStructure(T tree) {
+        assertNotNull(tree.getWidget(0));
+        assertTrue(tree.getWidget(0) instanceof MaterialTreeItem);
+        MaterialTreeItem item = (MaterialTreeItem) tree.getWidget(0);
+        assertTrue(item.getDivHeader().getElement().hasClassName(AddinsCssName.TREE_HEADER));
+        MaterialWidget divHeader = item.getDivHeader();
+        assertTrue(divHeader.getWidget(0) instanceof MaterialImage);
+        assertTrue(divHeader.getWidget(1) instanceof MaterialIcon);
+        assertTrue(divHeader.getWidget(2) instanceof Span);
     }
 
     @Override
     protected <T extends MaterialWidget> void checkChildren(T widget) {
-        // TODO Check specific children structure
+        RootPanel.get().add(widget);
+        for (int i = 1; i <= 5; i++) {
+            final String TEXT = "item" + i;
+            final String URL = "url" + i + ".png";
+            MaterialTreeItem item = new MaterialTreeItem();
+            item.setUrl(URL);
+            item.setIconType(IconType.POLYMER);
+            item.setText(TEXT);
+            widget.add(item);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            final String TEXT = "item" + (i + 1);
+            final String URL = "url" + (i + 1) + ".png";
+            MaterialTreeItem item = (MaterialTreeItem) widget.getWidget(i);
+            MaterialWidget divHeader = (MaterialWidget) item.getWidget(0);
+            // Check Url
+            assertEquals(item.getUrl(), URL);
+            assertTrue(divHeader.getWidget(0) instanceof MaterialImage);
+            MaterialImage image = (MaterialImage) divHeader.getWidget(0);
+            assertEquals(image.getUrl(), URL);
+            assertTrue(image.getElement().hasAttribute("src"));
+
+            // Check Icon
+            assertTrue(divHeader.getWidget(1) instanceof MaterialIcon);
+            MaterialIcon icon = (MaterialIcon) divHeader.getWidget(1);
+            assertEquals(icon.getIconType(), IconType.POLYMER);
+
+            // Check Text
+            assertEquals(item.getText(), TEXT);
+            assertTrue(divHeader.getWidget(2) instanceof Span);
+            Span span = (Span) divHeader.getWidget(2);
+            assertEquals(span.getText(), TEXT);
+        }
+        assertEquals(widget.getChildren().size(), 5);
     }
 }
