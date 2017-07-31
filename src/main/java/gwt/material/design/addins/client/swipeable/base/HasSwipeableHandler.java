f7df0cd606a3dc29ddaeaf39ed138f315e1d1abb
kevzlou7979 <kevzlou7979@gmail.com>
2017-07-31 09:42:07 +0800

    Fixed license headers

--- src/main/java/gwt/material/design/addins/client/swipeable/base/HasSwipeableHandler.java
+++ src/main/java/gwt/material/design/addins/client/swipeable/base/HasSwipeableHandler.java
@@ -2,7 +2,7 @@
  * #%L
  * GwtMaterial
  * %%
- * Copyright (C) 2015 - 2016 GwtMaterialDesign
+ * Copyright (C) 2015 - 2017 GwtMaterialDesign
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,8 @@
  */
 package gwt.material.design.addins.client.swipeable.base;
 
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.HasHandlers;
-import com.google.gwt.user.client.ui.Widget;
 import gwt.material.design.addins.client.swipeable.events.SwipeLeftEvent;
 import gwt.material.design.addins.client.swipeable.events.SwipeRightEvent;
 
