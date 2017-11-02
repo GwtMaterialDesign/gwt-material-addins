ae9e7be7bf42a52bd9e9f38144406d7aa576ce5f
kevzlou7979 <kevzlou7979@gmail.com>
2017-11-02 09:10:21 +0800

    Readd the AppLoadingState component from insclix core -> Incubator

---
+++ src/main/java/gwt/material/design/incubator/client/loadingstate/AppLoadingState.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * Errai Prototype
+ * %%
+ * Copyright (C) 2015 - 2017 Doltech
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package gwt.material.design.incubator.client.loadingstate;
+
+import com.google.gwt.user.client.ui.RootPanel;
+import gwt.material.design.client.base.MaterialWidget;
+import gwt.material.design.client.ui.animate.MaterialAnimation;
+import gwt.material.design.client.ui.animate.Transition;
+
+public class AppLoadingState extends LoadingStatePanel {
+
+    private MaterialWidget _target;
+
+    @Override
+    protected void onLoad() {
+        super.onLoad();
+
+        registerHandler(addLoadingHandler(event -> _target.setVisible(false)));
+        registerHandler(addSuccessHandler(event -> _target.setVisible(false)));
+        registerHandler(addErrorHandler(event -> reset(_target)));
+    }
+
+    public void reset(MaterialWidget target) {
+        new MaterialAnimation().transition(Transition.BOUNCEOUTDOWN).delay(3000).animate(this, () -> {
+            target.setVisible(true);
+            new MaterialAnimation().transition(Transition.BOUNCEINUP).animate(target, () -> {
+                RootPanel.get().getElement().getStyle().clearOverflow();
+                target.setVisible(true);
+            });
+            setVisible(false);
+        });
+    }
+
+    public MaterialWidget getTarget() {
+        return _target;
+    }
+
+    public void setTarget(MaterialWidget target) {
+        this._target = target;
+    }
+}
