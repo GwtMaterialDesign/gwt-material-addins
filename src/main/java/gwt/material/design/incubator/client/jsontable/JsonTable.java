697d2b62de1f98047947405d8f6d1c9144b3ccd8
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2018-01-29 03:58:28 +0800

    JsonTable - Add Selection Handler with Element as a generic param.

--- src/main/java/gwt/material/design/incubator/client/jsontable/JsonTable.java
+++ src/main/java/gwt/material/design/incubator/client/jsontable/JsonTable.java
@@ -21,16 +21,22 @@ package gwt.material.design.incubator.client.jsontable;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.json.client.JSONValue;
-import com.google.gwt.user.client.DOM;
 import gwt.material.design.client.MaterialDesign;
 import gwt.material.design.client.base.MaterialWidget;
+import gwt.material.design.client.ui.MaterialToast;
 import gwt.material.design.incubator.client.jsontable.js.JsTable;
 import gwt.material.design.incubator.client.jsontable.js.JsTableOptions;
+import gwt.material.design.jquery.client.api.JQueryElement;
 
 import static gwt.material.design.jquery.client.api.JQuery.$;
 
-public class JsonTable extends MaterialWidget {
+public class JsonTable extends MaterialWidget implements HasSelectionHandlers<Element> {
 
     static {
         MaterialDesign.injectJs(JsonTableClientBundle.INSTANCE.jsonTableJs());
@@ -48,5 +54,15 @@ public class JsonTable extends MaterialWidget {
         options.id = "#test";
         JsTable.jsontotable(value, options);
         $("table").addClass("striped");
+
+        $("tr").mousedown((e, handler) -> {
+            SelectionEvent.fire(JsonTable.this, $(e.target).parent().asElement());
+            return true;
+        });
+    }
+
+    @Override
+    public HandlerRegistration addSelectionHandler(SelectionHandler<Element> selectionHandler) {
+        return addHandler(selectionHandler, SelectionEvent.getType());
     }
 }
