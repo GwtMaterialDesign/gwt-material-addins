4bf48744a6fee7d365bcc605e1ccfd456d3505f3
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-03-15 10:08:13 +0800

    Update on DateOfBirthPicker

--- src/main/java/gwt/material/design/incubator/client/dob/DateOfBirthPicker.java
+++ src/main/java/gwt/material/design/incubator/client/dob/DateOfBirthPicker.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,15 +24,19 @@ import com.google.gwt.dom.client.Style;
 import gwt.material.design.addins.client.combobox.MaterialComboBox;
 import gwt.material.design.addins.client.inputmask.MaterialIntegerInputMask;
 import gwt.material.design.client.MaterialDesignBase;
-import gwt.material.design.client.base.*;
+import gwt.material.design.client.base.AbstractValueWidget;
+import gwt.material.design.client.base.HasFieldTypes;
+import gwt.material.design.client.base.HasReadOnly;
 import gwt.material.design.client.base.mixin.FieldTypeMixin;
-import gwt.material.design.client.constants.*;
-import gwt.material.design.client.ui.MaterialIcon;
+import gwt.material.design.client.constants.CssName;
+import gwt.material.design.client.constants.FieldType;
+import gwt.material.design.client.constants.StatusDisplayType;
+import gwt.material.design.client.constants.TextAlign;
 import gwt.material.design.incubator.client.AddinsIncubator;
 
 import java.util.Date;
 
-public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasFieldTypes, HasIcon, HasReadOnly, HasPlaceholder {
+public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasFieldTypes, HasReadOnly {
 
     static {
         if (AddinsIncubator.isDebug()) {
@@ -42,6 +46,7 @@ public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasF
         }
     }
 
+    private Date value;
     private MonthDataProvider dataProvider = new DefaultMonthDataProvider();
     private MaterialComboBox<Integer> month = new MaterialComboBox();
     private MaterialIntegerInputMask day = new MaterialIntegerInputMask();
@@ -77,7 +82,12 @@ public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasF
             }
         }
         month.setLabel("Birthdate");
-        month.setGrid("s6");
+        month.setGrid("s5");
+        month.addValueChangeHandler(event -> {
+            if (validateDate()) {
+                value.setMonth(month.getSingleValue());
+            }
+        });
     }
 
     protected void setupDay() {
@@ -86,7 +96,12 @@ public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasF
         day.setFloat(Style.Float.LEFT);
         day.setTextAlign(TextAlign.CENTER);
         day.setPlaceholder("DD");
-        day.setGrid("s2");
+        day.setGrid("s3");
+        day.addValueChangeHandler(event -> {
+            if (validateDate()) {
+                value.setDate(day.getValue());
+            }
+        });
     }
 
     protected void setupYear() {
@@ -96,21 +111,48 @@ public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasF
         year.setTextAlign(TextAlign.CENTER);
         year.setPlaceholder("YYYY");
         year.setGrid("s4");
+        year.addValueChangeHandler(event -> {
+            if (validateDate()) {
+                value.setYear(year.getValue() - 1900);
+            }
+        });
+    }
+
+    protected boolean validateDate() {
+        boolean valid = true;
+        if (!(month.getSingleValue() != null && month.getSingleValue() > 0 && month.getSingleValue() < 12)) {
+            valid = false;
+            month.setErrorText("Invalid Month");
+        } else {
+            month.clearErrorText();
+        }
+
+        if (!(year.getValue() != null && year.getValue() > 0)) {
+            valid = false;
+            year.setErrorText("Invalid Year");
+        } else {
+            year.clearErrorText();
+        }
+
+        if (!(day.getValue() != null && day.getValue() > 0 && day.getValue() <= 31)) {
+            valid = false;
+            day.setErrorText("Invalid Day");
+        } else {
+            day.clearErrorText();
+        }
+        return valid;
     }
 
     @Override
     public Date getValue() {
-        Integer yearValue = year.getValue();
-        Integer monthValue = month.getSingleValue();
-        Integer dayValue = day.getValue();
-        if (yearValue != null && monthValue != null && dayValue != null) {
-            return new Date(yearValue - 1900, monthValue, dayValue);
-        }
-        return null;
+        return value;
     }
 
     @Override
     public void setValue(Date value) {
+        super.setValue(value);
+
+        this.value = value;
         year.setValue(value.getYear() + 1900);
         day.setValue(value.getDay());
         month.setSingleValue(value.getMonth());
@@ -139,88 +181,40 @@ public class DateOfBirthPicker extends AbstractValueWidget<Date> implements HasF
     }
 
     @Override
-    public MaterialIcon getIcon() {
-        return null;
-    }
-
-    @Override
-    public void setIconType(IconType iconType) {
-
-    }
-
-    @Override
-    public void setIconPosition(IconPosition position) {
-
-    }
-
-    @Override
-    public void setIconSize(IconSize size) {
-
-    }
-
-    @Override
-    public void setIconFontSize(double size, Style.Unit unit) {
-
-    }
-
-    @Override
-    public void setIconColor(Color iconColor) {
-
-    }
-
-    @Override
-    public Color getIconColor() {
-        return null;
-    }
-
-    @Override
-    public void setIconPrefix(boolean prefix) {
-
-    }
-
-    @Override
-    public boolean isIconPrefix() {
-        return false;
-    }
-
-    @Override
-    public void setCustomIconType(String iconType) {
-
-    }
-
-    @Override
-    public String getCustomIconType() {
-        return null;
-    }
-
-    @Override
-    public String getPlaceholder() {
-        return null;
-    }
-
-    @Override
-    public void setPlaceholder(String placeholder) {
-
-    }
-
-    @Override
     public void setReadOnly(boolean value) {
-
+        month.setReadOnly(value);
+        year.setReadOnly(value);
+        day.setReadOnly(value);
     }
 
     @Override
     public boolean isReadOnly() {
-        return false;
+        return month.isReadOnly() && year.isReadOnly() && day.isReadOnly();
     }
 
     @Override
     public void setToggleReadOnly(boolean toggle) {
-
+        month.setToggleReadOnly(toggle);
+        year.setToggleReadOnly(toggle);
+        day.setToggleReadOnly(toggle);
     }
 
     @Override
     public boolean isToggleReadOnly() {
-        return false;
+        return month.isToggleReadOnly() && year.isToggleReadOnly() && day.isToggleReadOnly();
+    }
+
+    @Override
+    public void setStatusDisplayType(StatusDisplayType displayType) {
+        super.setStatusDisplayType(displayType);
+        month.setStatusDisplayType(displayType);
+        year.setStatusDisplayType(displayType);
+        day.setStatusDisplayType(displayType);
+    }
+
+    @Override
+    public StatusDisplayType getStatusDisplayType() {
+        return month.getStatusDisplayType();
     }
 
     public FieldTypeMixin<DateOfBirthPicker> getFieldTypeMixin() {
