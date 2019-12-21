6ac3a2ba0a2e2b7bf68eef0225a9937f750f9e5b
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-12-21 21:23:31 +0800

    Fixed Travis Build

--- src/test/java/gwt/material/design/addins/client/ui/MaterialRichEditorTest.java
+++ src/test/java/gwt/material/design/addins/client/ui/MaterialRichEditorTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,8 +26,8 @@ import com.google.gwt.event.shared.GwtEvent;
 import gwt.material.design.addins.client.base.constants.AddinsCssName;
 import gwt.material.design.addins.client.richeditor.MaterialRichEditor;
 import gwt.material.design.addins.client.richeditor.base.constants.ToolbarButton;
-import gwt.material.design.addins.client.richeditor.events.PasteEvent;
 import gwt.material.design.addins.client.ui.base.AbstractValueWidgetTest;
+import gwt.material.design.client.events.PasteEvent;
 
 /**
  * Test case for rich editor component
@@ -167,7 +167,8 @@ public class MaterialRichEditorTest extends AbstractValueWidgetTest<MaterialRich
         // Paste Event
         final boolean[] isPasteEventFired = {false};
         editor.addPasteHandler(event -> isPasteEventFired[0] = true);
-        editor.fireEvent(new PasteEvent() {});
+        editor.fireEvent(new PasteEvent("Test") {
+        });
         assertTrue(isPasteEventFired[0]);
         // Blur Event
         final boolean[] isBlurEventFired = {false};
@@ -177,7 +178,8 @@ public class MaterialRichEditorTest extends AbstractValueWidgetTest<MaterialRich
         // Focus Event
         final boolean[] isFocusEventFired = {false};
         editor.addFocusHandler(event -> isFocusEventFired[0] = true);
-        editor.fireEvent(new FocusEvent() {});
+        editor.fireEvent(new FocusEvent() {
+        });
         fireFocusEvent(editor);
         // KeyUp Event
         final boolean[] isKeyUpEventFired = {false};
