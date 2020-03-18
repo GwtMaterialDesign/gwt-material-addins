614f602cc08bce572ee0985299e2bdd09a5425d2
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-03-18 09:09:47 +0800

    MaterialDnd (InteractJs) upgrade version from 1.2.6 to 1.9.4 with new Resizable Feature.

---
+++ src/main/java/gwt/material/design/addins/client/dnd/constants/ResizeEvents.java
@@ -0,0 +1,6 @@
+package gwt.material.design.addins.client.dnd.constants;
+
+public interface ResizeEvents {
+
+    String RESIZE_MOVE = "resizemove";
+}
--- src/main/java/gwt/material/design/addins/client/dnd/js/Edge.java
+++ src/main/java/gwt/material/design/addins/client/dnd/js/Edge.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,35 +19,25 @@
  */
 package gwt.material.design.addins.client.dnd.js;
 
-import com.google.gwt.dom.client.Element;
-import gwt.material.design.jquery.client.api.JQueryElement;
-import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
- * JSInterop utils for dnd component
- *
- * @author kevzlou7979
+ * @author Mark Kevin
  */
-@JsType(isNative = true, namespace = JsPackage.GLOBAL)
-public class JsDnd extends JQueryElement {
+@JsType(isNative = true, name = "Object", namespace = JsPackage.GLOBAL)
+public class Edge {
 
-    @JsMethod(namespace = JsPackage.GLOBAL)
-    public static native JsDnd interact(Element target);
+    @JsProperty
+    public Object top;
 
-    @JsMethod
-    public native JsDnd draggable(JsDragOptions options);
+    @JsProperty
+    public Object left;
 
-    @JsMethod
-    public native JsDnd resizable(JsResizableOptions options);
+    @JsProperty
+    public Object bottom;
 
-    @JsMethod
-    public native JsDnd dropzone(JsDropOptions options);
-
-    @JsMethod
-    public native JsDnd ignoreFrom(String selector);
-
-    @JsMethod
-    public native JsDnd ignoreFrom(Element ignoreFrom);
+    @JsProperty
+    public Object right;
 }
