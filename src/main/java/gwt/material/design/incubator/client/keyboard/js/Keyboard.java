02bd757d64289f3074d102ed4c1b1d2c405a4783
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-03-19 18:12:27 +0800

    SimpleKeyboard
    - upgraded version from 2.21.3 to 2.28.0

--- src/main/java/gwt/material/design/incubator/client/keyboard/js/Keyboard.java
+++ src/main/java/gwt/material/design/incubator/client/keyboard/js/Keyboard.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -97,6 +97,12 @@ public class Keyboard {
     public native void setOptions(KeyboardOptions options);
 
     /**
+     * Removes keyboard listeners and DOM elements.
+     */
+    @JsMethod
+    public native void destroy();
+
+    /**
      * Send a command to all simple-keyboard instances at once (if you have multiple instances).
      */
     @JsMethod
@@ -118,9 +124,7 @@ public class Keyboard {
     /**
      * Removes an entry to the buttonTheme. Basically a way to remove a class previously added to a button through
      * buttonTheme or addButtonTheme.
-     *
-     * @param clasNames
      */
     @JsMethod
-    public native void removeButtonTheme(String buttons, String clasNames);
+    public native void removeButtonTheme(String buttons, String classNames);
 }
