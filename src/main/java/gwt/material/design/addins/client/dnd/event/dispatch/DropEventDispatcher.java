167fae087053d73a7d37d70fe98efebcedfeb97c
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-03-29 06:45:04 +0800

    Simplify and Fixed null source on DragEventDispatcher and DropEventDispatcher.

--- src/main/java/gwt/material/design/addins/client/dnd/event/dispatch/DropEventDispatcher.java
+++ src/main/java/gwt/material/design/addins/client/dnd/event/dispatch/DropEventDispatcher.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@
  */
 package gwt.material.design.addins.client.dnd.event.dispatch;
 
-import com.google.gwt.event.shared.GwtEvent;
 import gwt.material.design.client.base.MaterialWidget;
 import gwt.material.design.client.events.DropActivateEvent;
 import gwt.material.design.client.events.DropDeactivateEvent;
@@ -35,44 +34,14 @@ public class DropEventDispatcher {
     }
 
     public void fireDropActiveEvent() {
-        target.fireEvent(new GwtEvent<DropActivateEvent.DropActivateHandler>() {
-            @Override
-            public Type<DropActivateEvent.DropActivateHandler> getAssociatedType() {
-                return DropActivateEvent.TYPE;
-            }
-
-            @Override
-            protected void dispatch(DropActivateEvent.DropActivateHandler handler) {
-                handler.onDropActivate(new DropActivateEvent());
-            }
-        });
+        target.fireEvent(new DropActivateEvent());
     }
 
     public void fireDropEvent(JQueryElement relatedTarget) {
-        target.fireEvent(new GwtEvent<DropEvent.DropHandler>() {
-            @Override
-            public Type<DropEvent.DropHandler> getAssociatedType() {
-                return DropEvent.TYPE;
-            }
-
-            @Override
-            protected void dispatch(DropEvent.DropHandler handler) {
-                handler.onDrop(new DropEvent(relatedTarget));
-            }
-        });
+        target.fireEvent(new DropEvent(relatedTarget));
     }
 
     public void fireDropDeactivateEvent() {
-        target.fireEvent(new GwtEvent<DropDeactivateEvent.DropDeactivateHandler>() {
-            @Override
-            public Type<DropDeactivateEvent.DropDeactivateHandler> getAssociatedType() {
-                return DropDeactivateEvent.TYPE;
-            }
-
-            @Override
-            protected void dispatch(DropDeactivateEvent.DropDeactivateHandler handler) {
-                handler.onDropDeactivate(new DropDeactivateEvent());
-            }
-        });
+        target.fireEvent(new DropDeactivateEvent());
     }
 }
