ef176e8a15cdb42abe6cf88d8e53b1275ecd627a
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-07-13 13:56:08 +0800

    JsLiveStamp & Menubar code cleanups.

--- src/main/java/gwt/material/design/addins/client/menubar/MaterialMenuBar.java
+++ src/main/java/gwt/material/design/addins/client/menubar/MaterialMenuBar.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -85,9 +85,10 @@ public class MaterialMenuBar extends MaterialWidget {
     protected void onLoad() {
         super.onLoad();
 
-        $(getElement()).find(".dropdown-content li").css("minHeight", minHeight);
-        $(getElement()).find(".dropdown-content li").css("lineHeight", minHeight);
-        $(getElement()).find(".dropdown-content li").css("maxHeight", minHeight);
+        $(getElement()).find(".dropdown-content li")
+            .css("minHeight", minHeight)
+            .css("lineHeight", minHeight)
+            .css("maxHeight", minHeight);
     }
 
     public void setItemHeight(String minHeight) {
