9ca21e2a749bdb94e4864a5ea78d676c072964e3
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-10-03 07:13:03 +0800

    MaterialIconMorph - added reset method.

--- src/main/java/gwt/material/design/addins/client/iconmorph/MaterialIconMorph.java
+++ src/main/java/gwt/material/design/addins/client/iconmorph/MaterialIconMorph.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -84,10 +84,10 @@ public class MaterialIconMorph extends MaterialWidget implements HasDurationTran
     protected void onLoad() {
         super.onLoad();
 
-        addClickHandler(event -> {
+        registerHandler(addClickHandler(event -> {
             $(getElement()).toggleClass(MORPHED);
             IconMorphedEvent.fire(this, getElement().hasClassName(MORPHED));
-        });
+        }));
 
         // Check if we add the source and target icons thru ui binder
         if (source == null && target == null && getWidgetCount() == 2) {
@@ -112,6 +112,10 @@ public class MaterialIconMorph extends MaterialWidget implements HasDurationTran
 
         source.addStyleName(AddinsCssName.ICONS + " " + AddinsCssName.SOURCE);
         target.addStyleName(AddinsCssName.ICONS + " " + AddinsCssName.TARGET);
+    }
+
+    public void reset() {
+        getElement().removeClassName(MORPHED);
     }
 
     public void setIconSize(IconSize size) {
