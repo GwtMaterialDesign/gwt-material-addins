15881a7e9eff487948b78ab7142515d5c34526f0
kevzlou7979 <kevzlou7979@gmail.com>
2023-11-21 06:51:35 +0800

    MD5 Builder updates support HasDependency.

--- src/main/java/gwt/material/design/addins/client/md5/Md5Builder.java
+++ src/main/java/gwt/material/design/addins/client/md5/Md5Builder.java
@@ -2,14 +2,14 @@
  * #%L
  * GwtMaterial
  * %%
- * Copyright (C) 2015 - 2023 GwtMaterialDesign
+ * Copyright (C) 2015 - 2021 GwtMaterialDesign
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,31 +17,31 @@
  * limitations under the License.
  * #L%
  */
-package gwt.material.design.addins.client;
+package gwt.material.design.addins.client.md5;
 
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.resources.client.TextResource;
-import gwt.material.design.client.base.AbstractValueWidget;
 import gwt.material.design.client.base.HasDependency;
 import gwt.material.design.client.base.JsLoader;
 import gwt.material.design.client.base.mixin.DependencyCallback;
 import gwt.material.design.client.base.mixin.DependencyMixin;
 
-public abstract class AbstractAddinsValueWidget<T> extends AbstractValueWidget<T> implements HasDependency, JsLoader {
+public class Md5Builder implements HasDependency, JsLoader {
 
-    protected DependencyMixin dependencyMixin;
+    protected DependencyMixin<Md5Builder> dependencyMixin;
+    protected String message;
+    protected String converted = "";
+    protected Md5Function function;
 
-    public AbstractAddinsValueWidget(Element element) {
-        super(element);
+    public Md5Builder(String message, Md5Function function) {
+        this.message = message;
+        this.function = function;
+        install(Md5ClientBundle.INSTANCE.md5Js(), Md5DebugClientBundle.INSTANCE.md5DebugJs());
     }
 
-    public AbstractAddinsValueWidget(Element element, String... initialClass) {
-        super(element, initialClass);
-        getDependencyMixin().setDebug(MaterialAddins.isDebug());
-    }
-
-    public boolean isDependencyLoaded() {
-        return getDependencyMixin().isDependencyLoaded(getClass());
+    public Md5Builder(String message) {
+        this.message = message;
+        this.function = Md5Function.HEX;
+        install(Md5ClientBundle.INSTANCE.md5Js(), Md5DebugClientBundle.INSTANCE.md5DebugJs());
     }
 
     @Override
@@ -52,6 +52,45 @@ public abstract class AbstractAddinsValueWidget<T> extends AbstractValueWidget<T
     @Override
     public void install(TextResource minifiedJs, TextResource debugJs) {
         getDependencyMixin().install(minifiedJs, debugJs);
+    }
+
+    @Override
+    public void reload() {
+        unload();
+        load();
+    }
+
+    @Override
+    public void load() {
+        switch (function) {
+            case HEX:
+                converted = Md5.hex(message);
+                break;
+            case ARRAY:
+                converted = String.valueOf(Md5.array(message));
+                break;
+            case DIGEST:
+                converted = String.valueOf(Md5.digest(message));
+                break;
+            case ARRAY_BUFFER:
+                converted = (String) Md5.arrayBuffer(message);
+                break;
+            case BUFFER:
+                converted = (String) Md5.buffer(message);
+                break;
+            case BASE_64:
+                converted = Md5.base64(message);
+                break;
+        }
+    }
+
+    @Override
+    public void unload() {
+
+    }
+
+    public String convert() {
+        return converted;
     }
 
     @Override
@@ -74,9 +113,9 @@ public abstract class AbstractAddinsValueWidget<T> extends AbstractValueWidget<T
         getDependencyMixin().setDependencyLoaded(loaderClass, loaded);
     }
 
-    public DependencyMixin getDependencyMixin() {
+    public DependencyMixin<Md5Builder> getDependencyMixin() {
         if (dependencyMixin == null) {
-            dependencyMixin = new DependencyMixin(this);
+            dependencyMixin = new DependencyMixin<>(this);
         }
         return dependencyMixin;
     }
---
+++ src/main/java/gwt/material/design/addins/client/md5/Md5Function.java
@@ -0,0 +1,11 @@
+package gwt.material.design.addins.client.md5;
+
+public enum Md5Function {
+
+    HEX,
+    ARRAY,
+    DIGEST,
+    ARRAY_BUFFER,
+    BUFFER,
+    BASE_64
+}
